1eb19e243d1bad94652b1819ff27b69f482b3b8f
amadarain <t.f.octo@gmail.com>
2017-03-30 07:33:28 +0900

    [gradle] add config file support
    Read the library versions used in build.gradle from
    ~/.config/lazybones/am4dr-gradle-template-config.groovy if exists.

---
+++ templates/gradle/lazybones.groovy
@@ -0,0 +1,36 @@
+import java.nio.file.Files
+import java.nio.file.Paths
+
+def defaultConfig = new ConfigSlurper().parse( '''
+version {
+    kotlin = '1.1.1'
+    ben_manes_versions = '0.14.0'
+    groovy = '2.4.10'
+    commons_cli = '1.4'
+    slf4j = '1.7.25'
+    spock = '1.1-groovy-2.4-rc4'
+}
+''')
+
+def configPath = Paths.get(System.getProperty('user.home')).resolve('.config/lazybones/am4dr-gradle-template-config.groovy').toRealPath()
+def config = {
+    if (Files.exists(configPath)) {
+        println "info: config file found at: $configPath, use this"
+        defaultConfig.merge(new ConfigSlurper().parse(configPath.toFile().text))
+    }
+    else {
+        println "info: config file not found at $configPath, use default config"
+        defaultConfig
+    }
+}.call()
+def versions = config.version.with {[
+    KOTLIN_VERSION: kotlin,
+    BEN_MANES_VERSIONS_VERSION: ben_manes_versions,
+    GROOVY_VERSION: groovy,
+    COMMONS_CLI_VERSION: commons_cli,
+    SLF4J_VERSION: slf4j,
+    SPOCK_VERSION: spock,
+]}
+processTemplates('build.gradle', versions)
+
+
